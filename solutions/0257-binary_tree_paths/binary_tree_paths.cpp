504e62b54e2e4641a92b03e52946ec4f7fe3be64
fengjiran <fengjiran@foxmail.com>
2023-12-23 19:44:55 +0800

    leetcode 257

--- solutions/0257-binary_tree_paths/binary_tree_paths.cpp
+++ solutions/0257-binary_tree_paths/binary_tree_paths.cpp
@@ -68,17 +68,63 @@ namespace BinaryTreePaths {
     };
 }
 
-//TEST(Solution, binaryTreePaths) {
-//    BinaryTreePaths::Solution sln;
-//
-//    // case1
-//    std::vector<int> case1{0, null};
-//
-//}
-//
-//TEST(Solution, binaryTreePaths1) {
-//    //
-//}
+TEST(Solution, binaryTreePaths) {
+    BinaryTreePaths::Solution sln;
+
+    // case1
+    std::vector<int> nums1{1, 2, 3, null, 5};
+    std::vector<std::string> ans1{"1->2->5", "1->3"};
+    TreeNode *root1 = BuildBinaryTree(nums1);
+    std::vector<std::string> res1 = sln.binaryTreePaths(root1);
+    EXPECT_TRUE(Compare2DString(res1, ans1));
+
+    sln.resetMems();
+
+    // case2
+    std::vector<int> nums2{1};
+    std::vector<std::string> ans2{"1"};
+    TreeNode *root2 = BuildBinaryTree(nums2);
+    std::vector<std::string> res2 = sln.binaryTreePaths(root2);
+    EXPECT_TRUE(Compare2DString(res2, ans2));
+
+    sln.resetMems();
+
+    // case3
+    std::vector<int> nums3{1, 2, 3, 4, 5, 6, 7, 8, null, 10, 11, null, 13, null, 15};
+    std::vector<std::string> ans3{"1->2->4->8", "1->2->5->10", "1->2->5->11", "1->3->6->13", "1->3->7->15"};
+    TreeNode *root3 = BuildBinaryTree(nums3);
+    std::vector<std::string> res3 = sln.binaryTreePaths(root3);
+    EXPECT_TRUE(Compare2DString(res3, ans3));
+}
+
+TEST(Solution, binaryTreePaths1) {
+    BinaryTreePaths::Solution sln;
+
+    // case1
+    std::vector<int> nums1{1, 2, 3, null, 5};
+    std::vector<std::string> ans1{"1->2->5", "1->3"};
+    TreeNode *root1 = BuildBinaryTree(nums1);
+    std::vector<std::string> res1 = sln.binaryTreePaths1(root1);
+    EXPECT_TRUE(Compare2DString(res1, ans1));
+
+    sln.resetMems();
+
+    // case2
+    std::vector<int> nums2{1};
+    std::vector<std::string> ans2{"1"};
+    TreeNode *root2 = BuildBinaryTree(nums2);
+    std::vector<std::string> res2 = sln.binaryTreePaths1(root2);
+    EXPECT_TRUE(Compare2DString(res2, ans2));
+
+    sln.resetMems();
+
+    // case3
+    std::vector<int> nums3{1, 2, 3, 4, 5, 6, 7, 8, null, 10, 11, null, 13, null, 15};
+    std::vector<std::string> ans3{"1->2->4->8", "1->2->5->10", "1->2->5->11", "1->3->6->13", "1->3->7->15"};
+    TreeNode *root3 = BuildBinaryTree(nums3);
+    std::vector<std::string> res3 = sln.binaryTreePaths1(root3);
+    EXPECT_TRUE(Compare2DString(res3, ans3));
+}
 
 TEST(Solution, TestBinaryTreeConstructAndPrint) {
     GTEST_SKIP();
