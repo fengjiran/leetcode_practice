655e235c07226386fee884193fdd5fddeba23822
fengjiran <fengjiran@foxmail.com>
2023-12-24 20:07:19 +0800

    test leetcode0001 of two sum

---
+++ solutions/0001-two_sum/two_sum.cpp
@@ -0,0 +1,34 @@
+//
+// Created by 赵丹 on 2023/12/24.
+//
+#include "gtest/gtest.h"
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int> &nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (value2index.find(target - nums[i]) != value2index.end()) {
+                return {i, value2index[target - nums[i]]};
+            }
+            value2index[nums[i]] = i;
+        }
+        return {};
+    }
+
+private:
+    std::unordered_map<int, int> value2index;
+};
+
+TEST(Solution, twoSum) {
+    Solution sln;
+    // case1
+    std::vector<int> a{2, 7, 11, 15};
+    int target = 9;
+    std::vector<int> ans_a{0, 1};
+
+    std::vector<int> res_a = sln.twoSum(a, target);
+    EXPECT_EQ(res_a.size(), ans_a.size());
+    for (int item: res_a) {
+        EXPECT_TRUE(std::find(ans_a.begin(), ans_a.end(), item) != ans_a.end());
+    }
+}
