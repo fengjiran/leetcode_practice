84a0f94f5751f4ce55d0ff80a796ad6c0b5efb15
fengjiran <fengjiran@foxmail.com>
2023-12-28 22:44:58 +0800

    clang format

--- include/utils.h
+++ include/utils.h
@@ -5,32 +5,32 @@
 #ifndef LEETCODE_PRACTICE_UTILS_H
 #define LEETCODE_PRACTICE_UTILS_H
 
-#include <queue>
 #include "gtest/gtest.h"
+#include <queue>
 
 #define null INT32_MIN
 
 // Definition for a binary tree node.
 struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
 
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
 
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
 
-bool Compare2DVector(std::vector<std::vector<int>> &nums1, std::vector<std::vector<int>> &nums2);
+bool Compare2DVector(std::vector<std::vector<int>>& nums1, std::vector<std::vector<int>>& nums2);
 
-bool Compare2DString(std::vector<std::string> &str1, std::vector<std::string> &str2);
+bool Compare2DString(std::vector<std::string>& str1, std::vector<std::string>& str2);
 
 // construct binary tree from level order
 // ref:https://developer.aliyun.com/article/1217826
-TreeNode *BuildBinaryTree(const std::vector<int> &nums);
+TreeNode* BuildBinaryTree(const std::vector<int>& nums);
 
-void PrintBinaryTreeLevelOrder(TreeNode *root);
+void PrintBinaryTreeLevelOrder(TreeNode* root);
 
-#endif //LEETCODE_PRACTICE_UTILS_H
+#endif//LEETCODE_PRACTICE_UTILS_H
