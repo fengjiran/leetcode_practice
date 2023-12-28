96055a678d29c652066cac938b90a49b7d31b3f5
fengjiran <fengjiran@foxmail.com>
2023-12-28 17:05:17 +0800

    leetcode 200 of number of islands

--- solutions/0200-number_of_islands/number_of_islands.cpp
+++ solutions/0200-number_of_islands/number_of_islands.cpp
@@ -7,7 +7,54 @@ namespace NumberOfIslands {
 class Solution {
 public:
     int numIslands(std::vector<std::vector<char>>& grid) {
-        //
+        int res = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    DFS(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    void DFS(std::vector<std::vector<char>>& grid, int i, int j) {
+        int m = grid.size();
+        int n = grid[0].size();
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return;
+        }
+
+        if (grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+        DFS(grid, i, j - 1);
+        DFS(grid, i, j + 1);
+        DFS(grid, i - 1, j);
+        DFS(grid, i + 1, j);
     }
 };
 }// namespace NumberOfIslands
+
+TEST(Solution, numIslands) {
+    NumberOfIslands::Solution sln;
+
+    // case1
+    std::vector<std::vector<char>> grid1 = {{'1', '1', '1', '1', '0'},
+                                            {'1', '1', '0', '1', '0'},
+                                            {'1', '1', '0', '0', '0'},
+                                            {'0', '0', '0', '0', '0'}};
+    EXPECT_EQ(sln.numIslands(grid1), 1);
+
+    // case2
+    std::vector<std::vector<char>> grid2 = {{'1', '1', '0', '0', '0'},
+                                            {'1', '1', '0', '0', '0'},
+                                            {'0', '0', '1', '0', '0'},
+                                            {'0', '0', '0', '1', '1'}};
+    EXPECT_EQ(sln.numIslands(grid2), 3);
+}
