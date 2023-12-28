82f02f206c3a98375e8179d45e21e88d67e61684
fengjiran <fengjiran@foxmail.com>
2023-12-28 15:56:59 +0800

    leetcode 200 of number of islands

---
+++ solutions/0200-number_of_islands/number_of_islands.cpp
@@ -0,0 +1,13 @@
+//
+// Created by richard on 12/28/23.
+//
+#include "utils.h"
+
+namespace NumberOfIslands {
+class Solution {
+public:
+    int numIslands(std::vector<std::vector<char>>& grid) {
+        //
+    }
+};
+}// namespace NumberOfIslands
