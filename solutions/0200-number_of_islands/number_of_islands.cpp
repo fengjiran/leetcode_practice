4e1c5c5a39de9e7ab2147cf45dc0aeeafe66bc3a
fengjiran <fengjiran@foxmail.com>
2024-01-01 16:16:52 +0800

    leetcode 200 of number of islands using BFS

--- solutions/0200-number_of_islands/number_of_islands.cpp
+++ solutions/0200-number_of_islands/number_of_islands.cpp
@@ -6,7 +6,7 @@
 namespace NumberOfIslands {
 class Solution {
 public:
-    int numIslands(std::vector<std::vector<char>>& grid) {
+    int numIslandsDFS(std::vector<std::vector<char>>& grid) {
         int res = 0;
         int m = grid.size();
         int n = grid[0].size();
@@ -15,6 +15,21 @@ public:
                 if (grid[i][j] == '1') {
                     res++;
                     DFS(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    int numIslandsBFS(std::vector<std::vector<char>>& grid) {
+        int res = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    BFS(grid, i, j);
                 }
             }
         }
@@ -38,10 +53,36 @@ public:
         DFS(grid, i - 1, j);
         DFS(grid, i + 1, j);
     }
+
+    void BFS(std::vector<std::vector<char>>& grid, int i, int j) {
+        int m = grid.size();
+        int n = grid[0].size();
+        std::queue<std::pair<int, int>> q;
+        q.emplace(i, j);
+        while (!q.empty()) {
+            int sz = q.size();
+            for (int p = 0; p < sz; p++) {
+                std::pair<int, int> cur = q.front();
+                q.pop();
+                if (cur.first < 0 || cur.first >= m || cur.second < 0 || cur.second >= n) {
+                    continue;
+                }
+                if (grid[cur.first][cur.second] == '0') {
+                    continue;
+                }
+
+                grid[cur.first][cur.second] = '0';
+                q.emplace(cur.first, cur.second - 1);
+                q.emplace(cur.first, cur.second + 1);
+                q.emplace(cur.first - 1, cur.second);
+                q.emplace(cur.first + 1, cur.second);
+            }
+        }
+    }
 };
 }// namespace NumberOfIslands
 
-TEST(Solution, numIslands) {
+TEST(Solution, numIslandsDFS) {
     NumberOfIslands::Solution sln;
 
     // case1
@@ -49,12 +90,30 @@ TEST(Solution, numIslands) {
                                             {'1', '1', '0', '1', '0'},
                                             {'1', '1', '0', '0', '0'},
                                             {'0', '0', '0', '0', '0'}};
-    EXPECT_EQ(sln.numIslands(grid1), 1);
+    EXPECT_EQ(sln.numIslandsDFS(grid1), 1);
 
     // case2
     std::vector<std::vector<char>> grid2 = {{'1', '1', '0', '0', '0'},
                                             {'1', '1', '0', '0', '0'},
                                             {'0', '0', '1', '0', '0'},
                                             {'0', '0', '0', '1', '1'}};
-    EXPECT_EQ(sln.numIslands(grid2), 3);
+    EXPECT_EQ(sln.numIslandsDFS(grid2), 3);
+}
+
+TEST(Solution, numIslandsBFS) {
+    NumberOfIslands::Solution sln;
+
+    // case1
+    std::vector<std::vector<char>> grid1 = {{'1', '1', '1', '1', '0'},
+                                            {'1', '1', '0', '1', '0'},
+                                            {'1', '1', '0', '0', '0'},
+                                            {'0', '0', '0', '0', '0'}};
+    EXPECT_EQ(sln.numIslandsBFS(grid1), 1);
+
+    // case2
+    std::vector<std::vector<char>> grid2 = {{'1', '1', '0', '0', '0'},
+                                            {'1', '1', '0', '0', '0'},
+                                            {'0', '0', '1', '0', '0'},
+                                            {'0', '0', '0', '1', '1'}};
+    EXPECT_EQ(sln.numIslandsBFS(grid2), 3);
 }
