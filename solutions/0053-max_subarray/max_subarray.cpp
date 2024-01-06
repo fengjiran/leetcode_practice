892cdbab8d5165305a7ad3e54c6a32be2fc69e6f
fengjiran <fengjiran@foxmail.com>
2024-01-06 16:50:52 +0800

    leetcode 53 of max subarray

--- solutions/0053-max_subarray/max_subarray.cpp
+++ solutions/0053-max_subarray/max_subarray.cpp
@@ -50,6 +50,40 @@ public:
 
 
     // use pre sum
+    int maxSubArray2(std::vector<int>& nums) {
+        size_t n = nums.size();
+        std::vector<int> preSum(n + 1);
+        for (int i = 1; i <= n; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
+        }
+
+        int minVal = INT32_MAX;
+        int res = INT32_MIN;
+        for (int i = 0; i < n; i++) {
+            minVal = std::min(minVal, preSum[i]);
+            res = std::max(res, preSum[i + 1] - minVal);
+        }
+        return res;
+    }
+
+    int maxSubArray3(std::vector<int>& nums) {
+        size_t n = nums.size();
+        int left = 0;
+        int right = 0;
+        int windowSum = 0;
+        int maxSum = std::numeric_limits<int>::min();
+        while (right < n) {
+            windowSum += nums[right];
+            right++;
+            maxSum = std::max(maxSum, windowSum);
+            while (windowSum < 0) {
+                windowSum -= nums[left];
+                left++;
+            }
+        }
+
+        return maxSum;
+    }
 
     // use slide window
 };
@@ -62,14 +96,20 @@ TEST(Solution, maxSubArray) {
     std::vector<int> nums1{-2, 1, -3, 4, -1, 2, 1, -5, 4};
     EXPECT_EQ(sln.maxSubArray(nums1), 6);
     EXPECT_EQ(sln.maxSubArray1(nums1), 6);
+    EXPECT_EQ(sln.maxSubArray2(nums1), 6);
+    EXPECT_EQ(sln.maxSubArray3(nums1), 6);
 
     // case2
     std::vector<int> nums2{1};
     EXPECT_EQ(sln.maxSubArray(nums2), 1);
     EXPECT_EQ(sln.maxSubArray1(nums2), 1);
+    EXPECT_EQ(sln.maxSubArray2(nums2), 1);
+    EXPECT_EQ(sln.maxSubArray3(nums2), 1);
 
     // case3
     std::vector<int> nums3{5, 4, -1, 7, 8};
     EXPECT_EQ(sln.maxSubArray(nums3), 23);
     EXPECT_EQ(sln.maxSubArray1(nums3), 23);
+    EXPECT_EQ(sln.maxSubArray2(nums3), 23);
+    EXPECT_EQ(sln.maxSubArray3(nums3), 23);
 }
