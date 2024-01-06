ac37bd856901b932c27b23381ab32c9cdd451b5b
fengjiran <fengjiran@foxmail.com>
2024-01-06 15:23:57 +0800

    leetcode 53 of max subarray

--- solutions/0053-max_subarray/max_subarray.cpp
+++ solutions/0053-max_subarray/max_subarray.cpp
@@ -23,6 +23,31 @@ public:
         return res;
     }
 
+    int maxSubArray1(std::vector<int>& nums) {
+        size_t n = nums.size();
+        std::vector<int> memo(n, INT32_MAX);
+        int res = INT32_MIN;
+        for (int i = 0; i < n; i++) {
+            res = std::max(res, dp(nums, i, memo));
+        }
+        return res;
+    }
+
+    int dp(std::vector<int>& nums, int i, std::vector<int>& memo) {
+        if (i == 0) {
+            memo[i] = nums[i];
+            return memo[i];
+        }
+
+        if (memo[i] != INT32_MAX) {
+            return memo[i];
+        }
+
+        memo[i] = std::max(nums[i], dp(nums, i - 1, memo) + nums[i]);
+
+        return memo[i];
+    }
+
 
     // use pre sum
 
@@ -36,12 +61,15 @@ TEST(Solution, maxSubArray) {
     // case1
     std::vector<int> nums1{-2, 1, -3, 4, -1, 2, 1, -5, 4};
     EXPECT_EQ(sln.maxSubArray(nums1), 6);
+    EXPECT_EQ(sln.maxSubArray1(nums1), 6);
 
     // case2
     std::vector<int> nums2{1};
     EXPECT_EQ(sln.maxSubArray(nums2), 1);
+    EXPECT_EQ(sln.maxSubArray1(nums2), 1);
 
     // case3
     std::vector<int> nums3{5, 4, -1, 7, 8};
     EXPECT_EQ(sln.maxSubArray(nums3), 23);
+    EXPECT_EQ(sln.maxSubArray1(nums3), 23);
 }
