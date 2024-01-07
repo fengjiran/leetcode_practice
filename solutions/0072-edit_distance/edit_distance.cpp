26da2e7e38d1d6428381b84cd4dfbc5d303f0d43
fengjiran <fengjiran@foxmail.com>
2024-01-07 10:32:59 +0800

    leetcode 72 of edit distance

--- solutions/0072-edit_distance/edit_distance.cpp
+++ solutions/0072-edit_distance/edit_distance.cpp
@@ -6,11 +6,39 @@
 namespace EditDistance {
 class Solution {
 public:
+    // use recursion
     int minDistance(std::string& word1, std::string& word2) {
         int m = word1.size();
         int n = word2.size();
         std::vector<std::vector<int>> memo(m, std::vector<int>(n, -1));
         return dp(word1, m - 1, word2, n - 1, memo);
+    }
+
+    // use iteration(dp table)
+    int minDistance1(std::string& word1, std::string& word2) {
+        size_t m = word1.length();
+        size_t n = word2.length();
+        std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1));
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = std::min({dp[i - 1][j] + 1,
+                                         dp[i][j - 1] + 1,
+                                         dp[i - 1][j - 1] + 1});
+                }
+            }
+        }
+        return dp[m][n];
     }
 
     int dp(std::string& s1, int i, std::string& s2, int j, std::vector<std::vector<int>>& memo) {
@@ -45,6 +73,7 @@ TEST(Solution, minDistance) {
     std::string word1 = "horse";
     std::string word2 = "ros";
     EXPECT_EQ(sln.minDistance(word1, word2), 3);
+    EXPECT_EQ(sln.minDistance1(word1, word2), 3);
 
     // case2
 }
