c57f0095ef9e5833f5aa5541d5f8a5d7e817e40b
fengjiran <fengjiran@foxmail.com>
2024-01-08 09:23:53 +0800

    leetcode 1143 of lcs

--- solutions/1143-longest_common_subseq/longest_common_subseq.cpp
+++ solutions/1143-longest_common_subseq/longest_common_subseq.cpp
@@ -6,11 +6,36 @@
 namespace LCS {
 class Solution {
 public:
+    // use iteration
     int longestCommonSubsequence(std::string& text1, std::string& text2) {
         size_t m = text1.length();
         size_t n = text2.length();
         memo.resize(m, std::vector<int>(n, -1));
         return dp(text1, 0, text2, 0);
+    }
+
+    // use dp table
+    int LCS(std::string& text1, std::string& text2) {
+        size_t m = text1.length();
+        size_t n = text2.length();
+        std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1));
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1[i] == text2[j]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 
     int dp(std::string& s1, int i, std::string& s2, int j) {
@@ -42,4 +67,5 @@ TEST(Solution, longestCommonSubsequence) {
     std::string text1 = "abcde";
     std::string text2 = "ace";
     EXPECT_EQ(sln.longestCommonSubsequence(text1, text2), 3);
+    EXPECT_EQ(sln.LCS(text1, text2), 3);
 }
