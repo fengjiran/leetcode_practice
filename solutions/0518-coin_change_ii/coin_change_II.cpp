a4c7c08ab400a12e71bd74fc4c1757b4ce373dc2
fengjiran <fengjiran@foxmail.com>
2024-01-09 20:58:40 +0800

    leetcode 518 of coin change

---
+++ solutions/0518-coin_change_ii/coin_change_II.cpp
@@ -0,0 +1,44 @@
+//
+// Created by richard on 1/9/24.
+//
+#include "utils.h"
+
+namespace CoinChangeII {
+class Solution {
+public:
+    int change(int amount, std::vector<int>& coins) {
+        int n = coins.size();
+        std::vector<std::vector<int>> dp(n + 1, std::vector<int>(amount + 1));
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j < coins[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                }
+            }
+        }
+        return dp[n][amount];
+    }
+};
+}// namespace CoinChangeII
+
+TEST(Solution, change) {
+    CoinChangeII::Solution sln;
+
+    // case1
+    std::vector<int> nums1{1, 2, 5};
+    EXPECT_EQ(sln.change(5, nums1), 4);
+
+    // case2
+    std::vector<int> nums2{2};
+    EXPECT_EQ(sln.change(3, nums2), 0);
+
+    // case3
+    std::vector<int> nums3{10};
+    EXPECT_EQ(sln.change(10, nums3), 1);
+}
