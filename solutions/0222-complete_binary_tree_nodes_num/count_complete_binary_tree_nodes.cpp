1cbe940e8666dea3f818a83279c0a9707db5ed33
fengjiran <fengjiran@foxmail.com>
2024-01-12 21:38:03 +0800

    leetcode 222 of count complete binary tree nodes

--- solutions/0222-complete_binary_tree_nodes_num/count_complete_binary_tree_nodes.cpp
+++ solutions/0222-complete_binary_tree_nodes_num/count_complete_binary_tree_nodes.cpp
@@ -32,6 +32,32 @@ public:
 
         return countNodes(root->left) + countNodes(root->right) + 1;
     }
+
+    // traverse the binary tree with BFS to count nodes
+    int countNodesWithBFS(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+
+        std::queue<TreeNode*> q;
+        q.push(root);
+        int nums = 0;
+        while (!q.empty()) {
+            size_t sz = q.size();
+            for (int i = 0; i < sz; i++) {
+                TreeNode* cur = q.front();
+                q.pop();
+                nums++;
+                if (cur->left) {
+                    q.push(cur->left);
+                }
+                if (cur->right) {
+                    q.push(cur->right);
+                }
+            }
+        }
+        return nums;
+    }
 };
 }// namespace CompleteBinaryTreeNodes
 
@@ -42,14 +68,17 @@ TEST(Solution, countNodes) {
     std::vector<int> nums1{1, 2, 3, 4, 5, 6};
     TreeNode* root1 = BuildBinaryTree(nums1);
     EXPECT_EQ(sln.countNodes(root1), 6);
+    EXPECT_EQ(sln.countNodesWithBFS(root1), 6);
 
     // case2
     std::vector<int> nums2;
     TreeNode* root2 = BuildBinaryTree(nums2);
     EXPECT_EQ(sln.countNodes(root2), 0);
+    EXPECT_EQ(sln.countNodesWithBFS(root2), 0);
 
     // case3
     std::vector<int> nums3{1};
     TreeNode* root3 = BuildBinaryTree(nums3);
     EXPECT_EQ(sln.countNodes(root3), 1);
+    EXPECT_EQ(sln.countNodesWithBFS(root3), 1);
 }
