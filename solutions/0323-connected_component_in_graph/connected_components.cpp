d23598dd4b3793862194563acd9515c80e5f355e
fengjiran <fengjiran@foxmail.com>
2024-01-14 00:26:20 +0800

    leetcode 323 of count connected components

---
+++ solutions/0323-connected_component_in_graph/connected_components.cpp
@@ -0,0 +1,29 @@
+//
+// Created by 赵丹 on 2024/1/13.
+//
+#include "utils.h"
+
+namespace ConnectedComponent {
+class Solution {
+public:
+    int countComponents(int n, std::vector<std::vector<int>>& edges) {
+        UnionFind uf(n);
+        for (std::vector<int>& it: edges) {
+            uf.unionNodes(it[0], it[1]);
+        }
+        return uf.getCount();
+    }
+};
+}// namespace ConnectedComponent
+
+TEST(Solutionn, countComponents) {
+    ConnectedComponent::Solution sln;
+
+    // case1
+    std::vector<std::vector<int>> edges1{{0, 1}, {1, 2}, {3, 4}};
+    EXPECT_EQ(sln.countComponents(5, edges1), 2);
+
+    // case2
+    std::vector<std::vector<int>> edges2{{0, 1}, {1, 2}, {2, 3}, {3, 4}};
+    EXPECT_EQ(sln.countComponents(5, edges2), 1);
+}
