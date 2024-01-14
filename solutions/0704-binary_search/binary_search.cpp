335fbbf244a3e2aec02b4b7b2fe99449119c9296
fengjiran <fengjiran@foxmail.com>
2024-01-14 16:13:49 +0800

    leetcode 704 of binary search

--- solutions/0704-binary_search/binary_search.cpp
+++ solutions/0704-binary_search/binary_search.cpp
@@ -7,7 +7,7 @@ namespace BinarySearch {
 class Solution {
 public:
     // find left boundary
-    int searchLeftBoundary(std::vector<int>& nums, int target) {
+    int searchLeftBound(std::vector<int>& nums, int target) {
         int left = 0;
         int right = (int) nums.size();
         while (left < right) {
@@ -27,8 +27,24 @@ public:
         return nums[left] == target ? left : -1;
     }
 
-    int searchRightBoundary(std::vector<int>& nums, int target) {
-        //
+    int searchRightBound(std::vector<int>& nums, int target) {
+        int left = 0;
+        int right = (int) nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else if (nums[mid] == target) {
+                left = mid + 1;
+            }
+        }
+        if (left - 1 < 0) {
+            return -1;
+        }
+
+        return nums[left - 1] == target ? left - 1 : -1;
     }
 };
 }// namespace BinarySearch
@@ -39,10 +55,12 @@ TEST(Solution, binarySearch) {
     // case1
     std::vector<int> nums1{-1, 0, 3, 5, 9, 12};
     int target1 = 9;
-    EXPECT_EQ(sln.search(nums1, target1), 4);
+    EXPECT_EQ(sln.searchLeftBound(nums1, target1), 4);
+    EXPECT_EQ(sln.searchRightBound(nums1, target1), 4);
 
     // case2
     std::vector<int> nums2{-1, 0, 3, 5, 9, 12};
     int target2 = 2;
-    EXPECT_EQ(sln.search(nums2, target2), -1);
+    EXPECT_EQ(sln.searchLeftBound(nums2, target2), -1);
+    EXPECT_EQ(sln.searchRightBound(nums2, target2), -1);
 }
