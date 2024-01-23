efe2154d518389a74308fcd3632ac1cc9475d5f1
fengjiran <fengjiran@foxmail.com>
2024-01-24 00:03:11 +0800

    leetcode 1143

--- solutions/1143-longest_common_subseq/longest_common_subseq.cpp
+++ solutions/1143-longest_common_subseq/longest_common_subseq.cpp
@@ -19,12 +19,12 @@ public:
         size_t m = text1.length();
         size_t n = text2.length();
         std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1));
-        for (int i = 0; i <= m; i++) {
-            dp[i][0] = 0;
-        }
-        for (int j = 0; j <= n; j++) {
-            dp[0][j] = 0;
-        }
+//        for (int i = 0; i <= m; i++) {
+//            dp[i][0] = 0;
+//        }
+//        for (int j = 0; j <= n; j++) {
+//            dp[0][j] = 0;
+//        }
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
