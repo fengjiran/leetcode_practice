7bff35c9d26e3ed2c58dcac5a996b0639641106a
fengjiran <fengjiran@foxmail.com>
2024-01-24 21:28:21 +0800

    leetcode 198

--- solutions/0198-house_robber_i/house_robber_i.cpp
+++ solutions/0198-house_robber_i/house_robber_i.cpp
@@ -6,6 +6,7 @@
 namespace HouseRobberI {
 class Solution {
 public:
+    // use dp table
     int rob(std::vector<int>& nums) {
         int n = (int) nums.size();
         std::vector<int> dp(n + 1);
@@ -16,6 +17,31 @@ public:
         }
         return dp[n];
     }
+
+    // use recursive
+    int rob1(std::vector<int>& nums) {
+        int n = (int) nums.size();
+        memo.resize(n + 1, -1);
+        return dp(nums, n);
+    }
+
+    int dp(std::vector<int>& nums, int i) {
+        if (i == 0) {
+            return 0;
+        }
+        if (i == 1) {
+            return nums[0];
+        }
+
+        if (memo[i] != -1) {
+            return memo[i];
+        }
+
+        memo[i] = std::max(dp(nums, i - 1), dp(nums, i - 2) + nums[i - 1]);
+        return memo[i];
+    }
+
+    std::vector<int> memo;
 };
 }// namespace HouseRobberI
 
@@ -25,8 +51,11 @@ TEST(Solution, rob) {
     // case1
     std::vector<int> nums1{1, 2, 3, 1};
     EXPECT_EQ(sln.rob(nums1), 4);
+    EXPECT_EQ(sln.rob1(nums1), 4);
+    sln.memo.clear();
 
     // case2
     std::vector<int> nums2{2, 7, 9, 3, 1};
     EXPECT_EQ(sln.rob(nums2), 12);
+    EXPECT_EQ(sln.rob1(nums2), 12);
 }
