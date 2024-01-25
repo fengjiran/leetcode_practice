74c32dfb8b82c4c8d19daee74aecb3bc25c95c35
fengjiran <fengjiran@foxmail.com>
2024-01-26 01:05:56 +0800

    leetcode 10

--- solutions/0010-regex_match/regex_match.cpp
+++ solutions/0010-regex_match/regex_match.cpp
@@ -54,6 +54,40 @@ public:
         return memo[i][j];
     }
 
+    // use dp table
+    bool isMatch1(std::string& s, std::string& p) {
+        int m = static_cast<int>(s.length());
+        int n = static_cast<int>(p.length());
+        std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, -1));
+        dp[0][0] = true;
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = false;
+        }
+
+        for (int j = 1; j <= n; j++) {
+            if (j % 2 == 1) {
+                dp[0][j] = false;
+            } else {
+                dp[0][j] = dp[0][j - 2] && p[j - 1] == '*';
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    if (j - 1 < n && p[j - 1] == '*') {
+                        dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+                    } else {
+                        dp[i][j] = false;
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
     void resetMems() {
         memo.clear();
     }
@@ -70,16 +104,19 @@ TEST(Solution, isMatch) {
     std::string s1 = "aa";
     std::string p1 = "a";
     EXPECT_FALSE(sln.isMatch(s1, p1));
+    EXPECT_FALSE(sln.isMatch1(s1, p1));
     sln.resetMems();
 
     // case2
     std::string s2 = "aa";
     std::string p2 = "a*";
     EXPECT_TRUE(sln.isMatch(s2, p2));
+    EXPECT_TRUE(sln.isMatch1(s2, p2));
     sln.resetMems();
 
     // case3
     std::string s3 = "ab";
     std::string p3 = ".*";
     EXPECT_TRUE(sln.isMatch(s3, p3));
+    EXPECT_TRUE(sln.isMatch1(s3, p3));
 }
