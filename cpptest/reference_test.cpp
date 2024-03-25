dcca53b9ca2e6445d101c80cc0203f346166e61d
fengjiran <fengjiran@foxmail.com>
2024-03-25 14:06:58 +0800

    test

--- cpptest/reference_test.cpp
+++ cpptest/reference_test.cpp
@@ -7,7 +7,6 @@ namespace RefTest {
 void change(int&& ref) {
     ref = 8;
 }
-}// namespace RefTest
 
 TEST(RefTest, test1) {
     int a = 5;
@@ -18,14 +17,24 @@ TEST(RefTest, test1) {
     //    RefTest::change(leftRef);  // 编译不过，左值引用ref_a_left本身也是个左值
     //    RefTest::change(rightRef); // 编译不过，右值引用ref_a_right本身也是个左值
 
-    RefTest::change(std::move(a));
-    RefTest::change(std::move(leftRef));
-    RefTest::change(std::move(rightRef));
+    change(std::move(a));
+    change(std::move(leftRef));
+    change(std::move(rightRef));
 
-    RefTest::change(5);
+    change(5);
 
     std::cout << "Print address:\n";
     std::cout << "var: " << &a << std::endl;
     std::cout << "left ref: " << &leftRef << std::endl;
     std::cout << "right ref: " << &rightRef << std::endl;
+    EXPECT_EQ(&a, &leftRef);
+    EXPECT_EQ(&a, &rightRef);
+    ASSERT_EQ(&a, &leftRef);
+    ASSERT_EQ(&a, &rightRef);
 }
+
+TEST(RefTest, test2) {
+    //
+}
+
+}// namespace RefTest
