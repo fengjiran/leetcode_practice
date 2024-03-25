fb74339c1d0279cf384a01e023c9e0f86df6d78c
fengjiran <fengjiran@foxmail.com>
2024-03-26 02:16:08 +0800

    template test

--- cpptest/template_test.cpp
+++ cpptest/template_test.cpp
@@ -4,4 +4,35 @@
 #include "template_test.h"
 namespace TemplateTest {
 
+template<typename T>
+void Blob<T>::check(size_type i, const std::string& msg) const {
+    if (data->size() <= i) {
+        throw std::out_of_range(msg);
+    }
 }
+
+template<typename T>
+T& Blob<T>::back() {
+    check(0, "back on empty Blob.");
+    return data->back();
+}
+
+template<typename T>
+T& Blob<T>::front() {
+    check(0, "front on empty Blob.");
+    return data->front();
+}
+
+template<typename T>
+void Blob<T>::pop_back() {
+    check(0, "pop_back on empty Blob.");
+    data->pop_back();
+}
+
+template<typename T>
+T& Blob<T>::operator[](size_type i) {
+    check(i, "subscript out of range.");
+    return (*data)[i];
+}
+
+}// namespace TemplateTest
--- include/template_test.h
+++ include/template_test.h
@@ -6,4 +6,41 @@
 #define LEETCODE_PRACTICE_TEMPLATE_TEST_H
 #include "utils.h"
 
+namespace TemplateTest {
+
+template<typename T>
+class Blob {
+public:
+    //    typedef T value_type;
+    //    typedef typename std::vector<T>::size_type size_type;
+    using value_type = T;
+    using size_type = typename std::vector<T>::size_type;
+
+    // constructor
+    Blob();
+    Blob(std::initializer_list<T> il);
+
+    size_type size() const { return data->size(); }
+
+    bool empty() { return data->empty(); }
+
+    void push_back(const T& t) { data->push_back(t); }
+
+    void push_back(T&& t) { data->push_back(std::move(t)); }
+
+    void pop_back();
+
+    T& front();
+
+    T& back();
+
+    T& operator[](size_type i);
+
+private:
+    std::shared_ptr<std::vector<T>> data;
+    void check(size_type i, const std::string& msg) const;
+};
+
+}// namespace TemplateTest
+
 #endif//LEETCODE_PRACTICE_TEMPLATE_TEST_H
