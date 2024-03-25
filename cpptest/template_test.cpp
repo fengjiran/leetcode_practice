bdf415d244cd63b356f31fc1c55bd6e5ca9c2ceb
fengjiran <fengjiran@foxmail.com>
2024-03-25 21:30:13 +0800

    template test

--- cpptest/template_test.cpp
+++ cpptest/template_test.cpp
@@ -1,3 +1,7 @@
 //
 // Created by richard on 3/25/24.
 //
+#include "template_test.h"
+namespace TemplateTest {
+
+}
---
+++ include/template_test.h
@@ -0,0 +1,9 @@
+//
+// Created by richard on 3/25/24.
+//
+
+#ifndef LEETCODE_PRACTICE_TEMPLATE_TEST_H
+#define LEETCODE_PRACTICE_TEMPLATE_TEST_H
+#include "utils.h"
+
+#endif//LEETCODE_PRACTICE_TEMPLATE_TEST_H
