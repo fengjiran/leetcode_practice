c32b76db5b46a27ade144296b064ef3f5b389916
fengjiran <fengjiran@foxmail.com>
2024-03-27 00:27:38 +0800

    template test

--- cpptest/template_test.cpp
+++ cpptest/template_test.cpp
@@ -4,55 +4,14 @@
 #include "template_test.h"
 namespace TemplateTest {
 
-TEST(TemplateTest, test2) {
-    Blob<std::string> blob{"abc", "def"};
-}
-
-template<typename T>
-Blob<T>::Blob() : data(std::make_shared<std::vector<T>>()) {}
-
-template<typename T>
-Blob<T>::Blob(std::initializer_list<T> il) : data(std::make_shared<std::vector<T>>(il)) {}
-
-template<typename T>
-void Blob<T>::check(size_type i, const std::string& msg) const {
-    if (data->size() <= i) {
-        throw std::out_of_range(msg);
-    }
-}
-
-template<typename T>
-T& Blob<T>::back() {
-    check(0, "back on empty Blob.");
-    return data->back();
-}
-
-template<typename T>
-T& Blob<T>::front() {
-    check(0, "front on empty Blob.");
-    return data->front();
-}
-
-template<typename T>
-void Blob<T>::pop_back() {
-    check(0, "pop_back on empty Blob.");
-    data->pop_back();
-}
-
-template<typename T>
-T& Blob<T>::operator[](size_type i) {
-    check(i, "subscript out of range.");
-    return (*data)[i];
-}
-
 TEST(TemplateTest, test1) {
     std::cout << myMax(3, 7) << std::endl;
     std::cout << myMax(3.0, 7.0) << std::endl;
     std::cout << myMax('g', 'e') << std::endl;
 }
 
-//TEST(TemplateTest, test2) {
-//    Blob<std::string> blob{"abc", "def"};
-//}
+TEST(TemplateTest, test2) {
+    Blob<std::string> blob{"abc", "def"};
+}
 
 }// namespace TemplateTest
--- include/template_test.h
+++ include/template_test.h
@@ -46,6 +46,42 @@ private:
     void check(size_type i, const std::string& msg) const;
 };
 
+template<typename T>
+Blob<T>::Blob() : data(std::make_shared<std::vector<T>>()) {}
+
+template<typename T>
+Blob<T>::Blob(std::initializer_list<T> il) : data(std::make_shared<std::vector<T>>(il)) {}
+
+template<typename T>
+void Blob<T>::check(size_type i, const std::string& msg) const {
+    if (data->size() <= i) {
+        throw std::out_of_range(msg);
+    }
+}
+
+template<typename T>
+T& Blob<T>::back() {
+    check(0, "back on empty Blob.");
+    return data->back();
+}
+
+template<typename T>
+T& Blob<T>::front() {
+    check(0, "front on empty Blob.");
+    return data->front();
+}
+
+template<typename T>
+void Blob<T>::pop_back() {
+    check(0, "pop_back on empty Blob.");
+    data->pop_back();
+}
+
+template<typename T>
+T& Blob<T>::operator[](size_type i) {
+    check(i, "subscript out of range.");
+    return (*data)[i];
+}
 
 }// namespace TemplateTest
 
