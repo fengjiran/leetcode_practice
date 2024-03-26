d7c991ebe2f80a5a09721970265e18f53e66481f
fengjiran <fengjiran@foxmail.com>
2024-03-26 23:29:53 +0800

    test rvalue ref

--- cpptest/reference_test.cpp
+++ cpptest/reference_test.cpp
@@ -4,9 +4,27 @@
 #include "utils.h"
 
 namespace RefTest {
-
+void change(int&& ref) {
+    ref = 8;
 }
+}// namespace RefTest
 
 TEST(RefTest, test1) {
-    //
+    int a = 5;
+    int& leftRef = a;
+    int&& rightRef = std::move(a);
+
+    //    RefTest::change(a);  // 编译不过，a是左值，change参数要求右值
+    //    RefTest::change(leftRef);  // 编译不过，左值引用ref_a_left本身也是个左值
+    //    RefTest::change(rightRef); // 编译不过，右值引用ref_a_right本身也是个左值
+
+    RefTest::change(std::move(a));
+    RefTest::change(std::move(leftRef));
+    RefTest::change(std::move(rightRef));
+
+    RefTest::change(5);
+
+    std::cout << &a << std::endl;
+    std::cout << &leftRef << std::endl;
+    std::cout << &rightRef << std::endl;
 }
