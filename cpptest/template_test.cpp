8b2f90c3956243376852fa51ba8a95bef4878e17
fengjiran <fengjiran@foxmail.com>
2024-03-27 00:40:34 +0800

    template test

--- cpptest/template_test.cpp
+++ cpptest/template_test.cpp
@@ -5,13 +5,16 @@
 namespace TemplateTest {
 
 TEST(TemplateTest, test1) {
+    std::cout << "\nTemplateTest_test1:\n";
     std::cout << myMax(3, 7) << std::endl;
     std::cout << myMax(3.0, 7.0) << std::endl;
     std::cout << myMax('g', 'e') << std::endl;
 }
 
 TEST(TemplateTest, test2) {
+    std::cout << "\nTemplateTest_test2:\n";
     Blob<std::string> blob{"abc", "def"};
+    std::cout << "blob size: " << blob.size() << std::endl;
 }
 
 }// namespace TemplateTest
