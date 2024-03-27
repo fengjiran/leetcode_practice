8d9fdf078515edbc8311a35842ab9b6b4b8cc957
fengjiran <fengjiran@foxmail.com>
2024-03-28 00:33:43 +0800

    test

--- cpptest/dynamic_memory_test.cpp
+++ cpptest/dynamic_memory_test.cpp
@@ -3,7 +3,22 @@
 //
 #include "dynamic_memory_test.h"
 namespace DynamicMemoryTest {
+
 StrBlob::StrBlob() : data(std::make_shared<std::vector<std::string>>()) {}
+
+StrBlob::StrBlob(std::initializer_list<std::string> il) : data(std::make_shared<std::vector<std::string>>(il)) {}
+
+void StrBlob::check(StrBlob::size_type i, const std::string& msg) const {
+    if (i >= data->size()) {
+        throw std::out_of_range(msg);
+    }
+}
+
+void StrBlob::pop_back() {
+    check(0, "pop_back on empty StrBlob");
+    data->pop_back();
+}
+
 
 
 }// namespace DynamicMemoryTest
