1c487c6bf20d712acaa9226325ed6fce23c01631
fengjiran <fengjiran@foxmail.com>
2024-04-01 10:45:44 +0800

    test copy control

---
+++ cpptest/copy_control.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 赵丹 on 2024/4/1.
+//
+#include "utils.h"
+#include "copy_control.h"
+
+namespace CopyControlTest {
+
+}
---
+++ include/copy_control.h
@@ -0,0 +1,34 @@
+//
+// Created by 赵丹 on 2024/4/1.
+//
+
+#ifndef LEETCODE_PRACTICE_COPY_CONTROL_H
+#define LEETCODE_PRACTICE_COPY_CONTROL_H
+
+namespace CopyControlTest {
+class HasPtr {
+public:
+    explicit HasPtr(const std::string& s = std::string()) : ps(new std::string(s)), i(0) {}
+
+    HasPtr(const HasPtr& rhs) {
+        ps = new std::string(*rhs.ps);
+        i = rhs.i;
+    }
+
+    HasPtr& operator=(const HasPtr& rhs) {
+        if (this != &rhs) {
+            auto newps = new std::string(*rhs.ps);
+            delete ps;
+            ps = newps;
+            i = rhs.i;
+        }
+        return *this;
+    }
+
+private:
+    std::string* ps;
+    int i;
+};
+}// namespace CopyControlTest
+
+#endif//LEETCODE_PRACTICE_COPY_CONTROL_H
