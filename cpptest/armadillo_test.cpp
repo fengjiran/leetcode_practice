de2f3d6b2ef3c9a8002ab4535e8bd39cc85b2dc1
fengjiran <fengjiran@foxmail.com>
2024-04-18 04:50:51 +0800

    test

--- cpptest/armadillo_test.cpp
+++ cpptest/armadillo_test.cpp
@@ -12,4 +12,36 @@ TEST(ArmadilloTest, test1) {
     arma::mat B(4, 5, arma::fill::randu);
     std::cout << A * B.t() << std::endl;
 }
+
+TEST(ArmadilloTest, test2) {
+    std::cout << "\nArmadilloTest test_add:\n";
+    arma::fmat mat1 = "1,2,3;"
+                      "4,5,6;"
+                      "7,8,9";
+    arma::fmat mat2 = "1,2,3;"
+                      "4,5,6;"
+                      "7,8,9";
+
+    arma::fmat mat3 = "2,4,6;"
+                      "8,10,12;"
+                      "14,16,18";
+
+    //    ASSERT_EQ(arma::approx_equal(mat3, mat1 + mat2, "absdiff", 1e-5), true);
+    EXPECT_TRUE(arma::approx_equal(mat3, mat1 + mat2, "absdiff", 1e-5));
+}
+
+TEST(ArmadilloTest, test3) {
+    std::cout << "\nArmadilloTest test_sub:\n";
+    arma::fmat mat1 = "1,2,3;"
+                      "4,5,6;"
+                      "7,8,9";
+    arma::fmat mat2 = "1,2,3;"
+                      "4,5,6;"
+                      "7,8,9";
+
+    arma::fmat mat3 = "0,0,0;"
+                      "0,0,0;"
+                      "0,0,0";
+    EXPECT_TRUE(arma::approx_equal(mat3, mat1 - mat2, "absdiff", 1e-5));
+}
 }// namespace ArmadilloTest
